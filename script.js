1679cb4adc5f824260bf02498790506b3cbcd6ec
Dung Ly <ndungly@gmail.com>
2019-02-20 23:20:37 +0100

    First setup html and css - breakdown JS steps

---
+++ script.js
@@ -0,0 +1,19 @@
+const userInput
+const addToDo
+const toDoDone
+const removeToDo
+const toDoAllDone
+const toDoAllGone
+
+// userinput
+
+// addToDo function
+// add `<li class="to-do-item">${userInput}<button class="remove">x</button></li>` to ul. Remove placeholder text.
+
+// toDoDone - strikethrough text, add eventlistener on entire list item.
+
+// removeToDo - removes the parent of the targeted remove button including contents. Add eventlistener on the remove button.
+
+// toDoAllDone - when all done, give alert
+
+// toDoAllGone - when all gone, show text 'maybe you should add a to do?'.
