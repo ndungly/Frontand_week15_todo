30377763c87ee1a2c2c9d389ee5ec1b88875c2e6
Dung Ly <ndungly@gmail.com>
2019-02-21 17:53:43 +0100

    eerste JS

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
-const userInput
-const addToDo
+const todoInput = document.querySelector(".todo-input");
+const addToDo = document.querySelector(".add-todo");
+const removeToDo = document.querySelector(".remove");
+const todoList = document.querySelector(".to-do-list");
+const todoListItem = document.querySelector(".to-do-item");
+const emptyMsg = document.querySelector(".empty");
+
+/*
 const toDoDone
-const removeToDo
 const toDoAllDone
-const toDoAllGone
+*/
 
-// userinput
+console.dir(document.querySelector(".todo-input"));
 
-// addToDo function
-// add `<li class="to-do-item">${userInput}<button class="remove">x</button></li>` to ul. Remove placeholder text.
+// add a to do list item to the list, clear the input field and hide the empty message
+
+const addTodoItem = inputTxt => {
+    if (inputTxt === "") {
+        return;
+    }
+
+    todoList.innerHTML += `<li class="to-do-item">${inputTxt}<button class="remove">x</button></li>`;
+}
+
+// add `<li class="to-do-item">${todoInput}<button class="remove">x</button></li>` to ul. Remove placeholder text.
 
 // toDoDone - strikethrough text, add eventlistener on entire list item.
 
@@ -17,3 +31,12 @@ const toDoAllGone
 // toDoAllDone - when all done, give alert
 
 // toDoAllGone - when all gone, show text 'maybe you should add a to do?'.
+
+
+// EventListeners
+
+addToDo.addEventListener("click", () => {
+    addTodoItem(todoInput.value);
+    console.log('here');
+})
+
